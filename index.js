dcb01be618f917489beecf72ff75d194d2826962
Gabe Bravo <gabecbravo@gmail.com>
2017-01-23 17:31:13 -0500

    Added the initial db file to start up Mongo for routes.

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 require('dotenv').config();
 
 const server = require('./lib/server');
+const db     = require('./lib/db');
+
+db.connect((err) => {
+  if (err) {
+    return console.log(`DB is not working...`);
+  }
+  
   server.start();
+});

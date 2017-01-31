1d5ed605065deb97889048695adf01b4db06a353
Gabe Bravo <gabecbravo@gmail.com>
2017-01-30 21:20:05 -0500

    make it better

--- config.js
+++ config.js
@@ -1,4 +1,11 @@
 // module.exports = {
+//   port  : process.env.PORT || 3000,
+//   morgan: process.env.MORGAN || 'dev',
+//   mongo : process.env.MONGO || 'mongodb://surveyAdmin:bravo1@ds137729.mlab.com:37729/survey_db'
+// };
+
+
+// module.exports = {
 //   port  : process.env.PORT || 8080;
 //   morgan: process.env.MORGAN || 'dev',
 //   mongo : process.env.DATABASE_URL ||
@@ -8,9 +15,11 @@
 //                               'mongodb://localhost/survey_db')
 // };
 
+
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
                             'mongodb://surveyAdmin:bravo1@ds137729.mlab.com:37729/survey_db' :
                             'mongodb://localhost/survey_db');
 exports.PORT = process.env.PORT || 8080;
+exports.MORGAN = process.env.MORGAN || 'dev';

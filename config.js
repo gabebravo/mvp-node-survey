7ac5d5ffd4d55caadb71457666bc89547db03888
Gabe Bravo <gabecbravo@gmail.com>
2017-01-30 21:45:41 -0500

    make it better

--- config.js
+++ config.js
@@ -5,21 +5,20 @@
 // };
 
 
-module.exports = {
-  port  : process.env.PORT || 8080;
-  morgan: process.env.MORGAN || 'dev',
-  mongo : process.env.DATABASE_URL ||
-                         global.DATABASE_URL ||
-                         (process.env.NODE_ENV === 'production' ?
-                              'mongodb://surveyAdmin:bravo1@ds137729.mlab.com:37729/survey_db' :
-                              'mongodb://localhost/survey_db')
-};
+// module.exports = {
+//   port  : process.env.PORT || 8080;
+//   morgan: process.env.MORGAN || 'dev',
+//   mongo : process.env.DATABASE_URL ||
+//                          global.DATABASE_URL ||
+//                          (process.env.NODE_ENV === 'production' ?
+//                               'mongodb://surveyAdmin:bravo1@ds137729.mlab.com:37729/survey_db' :
+//                               'mongodb://localhost/survey_db')
+// };
 
-
-// exports.DATABASE_URL = process.env.DATABASE_URL ||
-//                        global.DATABASE_URL ||
-//                        (process.env.NODE_ENV === 'production' ?
-//                             'mongodb://surveyAdmin:bravo1@ds137729.mlab.com:37729/survey_db' :
-//                             'mongodb://localhost/survey_db');
-// exports.PORT = process.env.PORT || 8080;
-// exports.MORGAN = process.env.MORGAN || 'dev';
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+                       global.DATABASE_URL ||
+                       (process.env.NODE_ENV === 'production' ?
+                            'mongodb://surveyAdmin:bravo1@ds137729.mlab.com:37729/survey_db' :
+                            'mongodb://localhost/survey_db');
+exports.PORT = process.env.PORT || 8080;
+exports.MORGAN = process.env.MORGAN || 'dev';

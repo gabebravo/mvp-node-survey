d2d4f6d3c66f2c28bbbaee5b99a0fba2d7b0a5d7
Gabe Bravo <gabecbravo@gmail.com>
2017-01-30 21:33:10 -0500

    Modified the db in the config file.

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
-module.exports = {
-  port  : process.env.PORT || 3000,
-  morgan: process.env.MORGAN || 'dev',
-  mongo : process.env.MONGO || 'mongodb://localhost:27017/testSurvey'
-};
-
-
 // module.exports = {
-//   port  : process.env.PORT || 8080;
+//   port  : process.env.PORT || 3000,
 //   morgan: process.env.MORGAN || 'dev',
-//   mongo : process.env.DATABASE_URL ||
-//                          global.DATABASE_URL ||
-//                          (process.env.NODE_ENV === 'production' ?
-//                               'mongodb://surveyAdmin:bravo1@ds137729.mlab.com:37729/survey_db' :
-//                               'mongodb://localhost/survey_db')
+//   mongo : process.env.MONGO || 'mongodb://localhost:27017/testSurvey'
 // };
+
+
+module.exports = {
+  port  : process.env.PORT || 8080;
+  morgan: process.env.MORGAN || 'dev',
+  mongo : process.env.DATABASE_URL ||
+                         global.DATABASE_URL ||
+                         (process.env.NODE_ENV === 'production' ?
+                              'mongodb://surveyAdmin:bravo1@ds137729.mlab.com:37729/survey_db' :
+                              'mongodb://localhost/survey_db')
+};
 
 
 // exports.DATABASE_URL = process.env.DATABASE_URL ||

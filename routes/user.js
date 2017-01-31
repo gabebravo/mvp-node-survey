3a987af22871f30a0f2e21bbd1eb6a64580a54c2
Gabe Bravo <gabecbravo@gmail.com>
2017-01-30 21:28:51 -0500

    Modified configs

--- routes/user.js
+++ routes/user.js
@@ -7,9 +7,9 @@ const User = db.mongoose.model('User');
 const jwt = require('jsonwebtoken');
 const SECRET = process.env.JWT_SECRET;
 
-const email = require("../node_modules/emailjs/email");
-const USER = process.env.EMAILJS_USER;
-const PASSWORD = process.env.EMAILJS_PASSWORD;
+// const email = require("../node_modules/emailjs/email");
+// const USER = process.env.EMAILJS_USER;
+// const PASSWORD = process.env.EMAILJS_PASSWORD;
 
 const uuid = require('uuid');
 
@@ -84,52 +84,52 @@ function resetPassword(req, res) {
   });
 }
 
-emailUser = (req, res) => {
-  User.findOne({
-    email: req.body.email
-  }, 'name')
-  .exec()
-  .then( user => {
-
-    let tempPass = uuid.v4();
-
-    let username = user.name;
-    let server  = email.server.connect({
-       user:    USER,
-       password: PASSWORD,
-       host:    "smtp-mail.outlook.com",
-       tls: {ciphers: "SSLv3"}
-    });
-
-    var message = {
-       text:    "i hope this works",
-       from:    "Admin <gabriel.bravo1@outlook.com>",
-       to:      "Gabe <gabecbravo@gmail.com>",
-       cc:      "",
-       subject: "testing emailjs",
-       attachment:
-       [
-          {data:'<html>i <i>hope</i> this works! Follow this <a href="heroku.com/name/user/reset/'+ tempPass +'">link</a></html>', alternative:true}
-       ]
-    };
-  server.send(message, function(err, message) { console.log(err || message); });
-
-  user.update({reset: tempPass}, function (err) {
-      if(err) {
-        res.status(200).json({message: 'Internal server error'});
-      }
-        res.status(200).json({message: 'An reset email was sent to you.'});
-   });
-
-  })
-  .catch( err => {
-    res.status(500).send(err);
-  });
-}
+// emailUser = (req, res) => {
+//   User.findOne({
+//     email: req.body.email
+//   }, 'name')
+//   .exec()
+//   .then( user => {
+//
+//     let tempPass = uuid.v4();
+//
+//     let username = user.name;
+//     let server  = email.server.connect({
+//        user:    USER,
+//        password: PASSWORD,
+//        host:    "smtp-mail.outlook.com",
+//        tls: {ciphers: "SSLv3"}
+//     });
+//
+//     var message = {
+//        text:    "i hope this works",
+//        from:    "Admin <gabriel.bravo1@outlook.com>",
+//        to:      "Gabe <gabecbravo@gmail.com>",
+//        cc:      "",
+//        subject: "testing emailjs",
+//        attachment:
+//        [
+//           {data:'<html>i <i>hope</i> this works! Follow this <a href="heroku.com/name/user/reset/'+ tempPass +'">link</a></html>', alternative:true}
+//        ]
+//     };
+//   server.send(message, function(err, message) { console.log(err || message); });
+//
+//   user.update({reset: tempPass}, function (err) {
+//       if(err) {
+//         res.status(200).json({message: 'Internal server error'});
+//       }
+//         res.status(200).json({message: 'An reset email was sent to you.'});
+//    });
+//
+//   })
+//   .catch( err => {
+//     res.status(500).send(err);
+//   });
+// }
 
 router.post('/authenticate', userLogin);
 router.get('/',    getUsers);
 router.post('/',   createUser);
-router.post('/email', emailUser);
+// router.post('/email', emailUser);
 
 module.exports = router;
